124e0f96b7ebf17fbe27a24ccd1e1067434fff07
jonas <jhulthen@kth.se>
2023-01-26 16:12:16 +0100

    feat, added LIC 0-3

--- src/main/Input.java
+++ src/main/Input.java
@@ -36,4 +36,3 @@ public class Input {
         return PUV;
     }
 }
-
--- src/main/LaunchInterceptorConditions.java
+++ src/main/LaunchInterceptorConditions.java
@@ -1,79 +1,98 @@
 package main;
 
 public class LaunchInterceptorConditions {
-    public static boolean ConditionZero(double LENGTH1, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionZero(double LENGTH1, int[][] POINTS) {
+        for (int i = 0; i < POINTS.length-1; i++) {
+            if (HelperFunction.calculateDistance(POINTS[i], POINTS[i + 1]) > LENGTH1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean ConditionOne(double RADIUS1, int[][] POINTS) {
+        
+        for (int i = 0; i < POINTS.length-2; i++) {
+            if (!HelperFunction.checkTriangelFitCircle(POINTS[i], POINTS[i + 1], POINTS[i + 2], RADIUS1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean Conditiontwo(double EPSILON, int[][] POINTS) {
+        for (int i = 0; i < POINTS.length-2; i++) {
+            if (HelperFunction.checkAngle(POINTS[i], POINTS[i + 1], POINTS[i + 2], EPSILON)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean ConditionThree(double AREA1, int[][] POINTS) {
+        for (int i = 0; i < POINTS.length-2; i++) {
+            if (HelperFunction.checkArea(POINTS[i], POINTS[i + 1], POINTS[i + 2], AREA1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean ConditionFour(int QPTS, int QUADS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionOne(double RADIUS1, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionFive(int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean Conditiontwo(double EPSILON, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionSix(int NPTS, double DIST, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionThree(double AREA1, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionSeven(double LENGTH1, int KPTS, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionFour(int QPTS, int QUADS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionEight(int APTS, int BPTS, double RADIUS1, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionFive(int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionNine(int CPTS, int DPTS, double EPSILON, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionSix(int NPTS, double DIST, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionTen(int EPTS, int FPTS, double AREA1, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionSeven(double LENGTH1,int KPTS,int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionEleven(int GPTS, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionEight(int APTS, int BPTS, double RADIUS1, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionTwelve(int KPTS, double LENGTH2, int NUMPOINTS, int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionNine(int CPTS, int DPTS, double EPSILON, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionThirteen(int APTS, int BPTS, double RADIUS1, double RADIUS2, int NUMPOINTS,
+            int[][] POINTS) {
+        // do something
         return true;
     }
-    public static boolean ConditionTen(int EPTS, int FPTS, double AREA1, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
-        return true;
-    }
-    public static boolean ConditionEleven(int GPTS, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
-        return true;
-    }
-    public static boolean ConditionTwelve(int KPTS, double LENGTH2, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
-        return true;
-    }
-    public static boolean ConditionThirteen(int APTS, int BPTS, double RADIUS1, double RADIUS2, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
-        return true;
-    }
-    public static boolean ConditionFourteen(int EPTS, int FPTS, double AREA1, double AREA2, int NUMPOINTS, int[][] POINTS)
-    {
-        //do something
+
+    public static boolean ConditionFourteen(int EPTS, int FPTS, double AREA1, double AREA2, int NUMPOINTS,
+            int[][] POINTS) {
+        // do something
         return true;
     }
 }
