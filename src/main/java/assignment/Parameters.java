4df7beb9bab0da54c1b3c3ef926c5d9d3fcd075e
Aleksey Veresov <aleksey@veresov.pro>
2023-01-29 14:28:14 +0100

    Change Parameters' fields to be public final

--- src/main/java/assignment/Parameters.java
+++ src/main/java/assignment/Parameters.java
@@ -1,25 +1,25 @@
 package assignment;
 
 public class Parameters {
-    private double LENGTH1;
-    private double RADIUS1;
-    private double EPSILON;
-    private double AREA1;
-    private int QPTS;
-    private int QUADS;
-    private double DIST;
-    private int NPTS;
-    private int KPTS;
-    private int APTS;
-    private int BPTS;
-    private int CPTS;
-    private int DPTS;
-    private int EPTS;
-    private int FPTS;
-    private int GPTS;
-    private double LENGTH2;
-    private double RADIUS2;
-    private double AREA2;
+    public final double LENGTH1;
+    public final double RADIUS1;
+    public final double EPSILON;
+    public final double AREA1;
+    public final int QPTS;
+    public final int QUADS;
+    public final double DIST;
+    public final int NPTS;
+    public final int KPTS;
+    public final int APTS;
+    public final int BPTS;
+    public final int CPTS;
+    public final int DPTS;
+    public final int EPTS;
+    public final int FPTS;
+    public final int GPTS;
+    public final double LENGTH2;
+    public final double RADIUS2;
+    public final double AREA2;
 
     public Parameters(double LENGTH1, double RADIUS1, double EPSILON, double AREA1, int QPTS,
                     int QUADS, double DIST, int NPTS, int KPTS, int APTS, int BPTS, int CPTS,
@@ -44,100 +44,5 @@ public class Parameters {
         this.LENGTH2 = LENGTH2;
         this.RADIUS2 = RADIUS2;
         this.AREA2 = AREA2;
-    }
-
-    // write getters
-    public double getLENGTH1() {
-        return LENGTH1;
-    }
-
-    // write getters
-    public double getRADIUS1() {
-        return RADIUS1;
-    }
-
-    // write getters
-    public double getEPSILON() {
-        return EPSILON;
-    }
-
-    // write getters
-    public double getAREA1() {
-        return AREA1;
-    }
-
-    // write getters
-    public int getQPTS() {
-        return QPTS;
-    }
-
-    // write getters
-    public int getQUADS() {
-        return QUADS;
-    }
-
-    // write getters
-    public double getDIST() {
-        return DIST;
-    }
-
-    // write getters
-    public int getNPTS() {
-        return NPTS;
-    }
-
-    // write getters
-    public int getKPTS() {
-        return KPTS;
-    }
-
-    // write getters
-    public int getAPTS() {
-        return APTS;
-    }
-
-    // write getters
-    public int getBPTS() {
-        return BPTS;
-    }
-
-    // write getters
-    public int getCPTS() {
-        return CPTS;
-    }
-
-    // write getters
-    public int getDPTS() {
-        return DPTS;
-    }
-
-    // write getters
-    public int getEPTS() {
-        return EPTS;
-    }
-
-    // write getters
-    public int getFPTS() {
-        return FPTS;
-    }
-
-    // write getters
-    public int getGPTS() {
-        return GPTS;
-    }
-
-    // write getters
-    public double getLENGTH2() {
-        return LENGTH2;
-    }
-
-    // write getters
-    public double getRADIUS2() {
-        return RADIUS2;
-    }
-
-    // write getters
-    public double getAREA2() {
-        return AREA2;
     }
 }
