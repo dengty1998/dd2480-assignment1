07bf87c3a88bb718979b35bf40ad2f533ed3fe8b
Aleksey Veresov <aleksey@veresov.pro>
2023-01-29 14:40:21 +0100

    Move Connect into Input
    Fixes #38

--- src/main/java/assignment/Input.java
+++ src/main/java/assignment/Input.java
@@ -2,11 +2,15 @@ package assignment;
 
 // Input for the decide function
 public class Input {
-    private int NUMPOINTS;
-    private int[][] POINTS;
-    private Parameters PARAMETERS;
-    private Connect[][] LCM;
-    private boolean[] PUV;
+    public final int NUMPOINTS;
+    public final int[][] POINTS;
+    public final Parameters PARAMETERS;
+    public final Connect[][] LCM;
+    public final boolean[] PUV;
+
+    public static enum Connect {
+        ANDD, ORR, NOTUSED;
+    }
 
     public Input(int NUMPOINTS, int[][] POINTS, Parameters PARAMETERS, Connect[][] LCM,
                     boolean[] PUV) {
@@ -15,25 +19,5 @@ public class Input {
         this.PARAMETERS = PARAMETERS;
         this.LCM = LCM;
         this.PUV = PUV;
-    }
-
-    public int getNUMPOINTS() {
-        return NUMPOINTS;
-    }
-
-    public int[][] getPOINTS() {
-        return POINTS;
-    }
-
-    public Parameters getPARAMETERS() {
-        return PARAMETERS;
-    }
-
-    public Connect[][] getLCM() {
-        return LCM;
-    }
-
-    public boolean[] getPUV() {
-        return PUV;
     }
 }
